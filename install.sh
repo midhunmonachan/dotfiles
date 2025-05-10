73a4eac30ff63fa91c545251421a052802e53427
Midhun Monachan <midhunmonachan@gmail.com>
2025-05-10 08:15:39 -0400

    Improved Script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 declare -A SUPPORTED_OS
 SUPPORTED_OS["ubuntu"]="24.04"
 
-log() { local l=${1^^} m=$2 c; case $l in INFO)c="30;42";;WARNING)c="30;43";;ERROR)c="30;41";;*)echo -e "[$l] $m";return;;esac; echo -e "\\033[${c}m $l \\033[0m $m"; }
+log() { local c; case ${1^^} in INFO)c="30;42";;WARNING)c="30;43";;ERROR)c="30;41";;*)echo -e "[${1^^}] $2";return;;esac; echo -e "\\033[${c}m ${1^^} \\033[0m $2"; }
 
 check_os() { source /etc/os-release 2>/dev/null; log INFO "${PRETTY_NAME:-Unknown OS} detected."; [[ -v SUPPORTED_OS[${ID:-unknown}] && ",${SUPPORTED_OS[${ID:-unknown}]}," == *",${VERSION_ID:-unknown},"* ]] || { read -r -p "$(log WARNING "Unsupported OS. Continue? (y/n)") " confirm; [[ "$confirm" =~ ^[Yy]$ ]] || { log ERROR "Aborted by user."; exit 1; }; }; }
 
@@ -14,13 +14,18 @@ install_dependencies() {
 
 install_dotfiles() {
 	log INFO "Installing dotfiles...";
-	if [[ -d "$HOME/.dotfiles" ]]; then
-		log INFO "Existing .dotfiles directory found. Removing it before fresh installation."
-		rm -rf "$HOME/.dotfiles" || { log ERROR "Failed to remove existing .dotfiles directory."; exit 1; }
-	fi
-	git clone https://github.com/midhunmonachan/dotfiles.git "$HOME/.dotfiles" || { log ERROR "Failed to clone dotfiles repository."; exit 1; }
+	(
+	  cd "$HOME" || { log ERROR "Failed to change to $HOME directory. Aborting installation."; exit 1; }
 
-	rm -f "$HOME/.dotfiles/README.md" && rm -f "$HOME/.dotfiles/install.sh"
+	  if [[ -d ".dotfiles" ]]; then
+		  log INFO "Existing .dotfiles directory found. Removing it before fresh installation."
+		  rm -rf ".dotfiles" || { log ERROR "Failed to remove existing .dotfiles directory."; exit 1; }
+	  fi
+
+	  git clone https://github.com/midhunmonachan/dotfiles.git ".dotfiles" || { log ERROR "Failed to clone dotfiles repository."; exit 1; }
+
+	  rm -f ".dotfiles/README.md" && rm -f ".dotfiles/install.sh"
+	) || exit 1
 }
 
 check_os
